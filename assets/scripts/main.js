7dc8009a092c0ab8b8f05003ae22d32b6942907c
zhongtiff <tzhong@ucsd.edu>
2021-11-07 22:12:44 -0800

    commit to main

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -56,7 +56,52 @@ function initializeServiceWorker() {
   /**
    *  TODO - Part 2 Step 1
    *  Initialize the service worker set up in sw.js
+   * 
    */
+   if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('/sw.js').then(function(registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+  }
+  self.addEventListener('install', function(event) {
+    var CACHE_NAME = 'my-site-cache-v1';
+    var urlsToCache = [
+      '/',
+      '/styles/main.css',
+      '/script/main.js'
+    ];
+    
+    self.addEventListener('install', function(event) {
+      // Perform install steps
+      event.waitUntil(
+        caches.open(CACHE_NAME)
+          .then(function(cache) {
+            console.log('Opened cache');
+            return cache.addAll(urlsToCache);
+          })
+      );
+    });
+  });
+  
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(response) {
+          // Cache hit - return response
+          if (response) {
+            return response;
+          }
+          return fetch(event.request);
+        }
+      )
+    );
+  });
 }
 
 /**
